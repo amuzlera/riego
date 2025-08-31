fe52a38bc7e30da621b5e4deb498e243d45f2f09
amuzlera <71038398+amuzlera@users.noreply.github.com>
2025-08-31 20:02:28 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,13 +1,15 @@
 import machine
 import time
 
-LED_PIN = 2  # Cambiá si usás otro pin
+LED_PIN = 2
 led = machine.Pin(LED_PIN, machine.Pin.OUT)
 
-print("task.py ejecutando: parpadeo LED")
+print("task.py ejecutando: parpadeo LED por 10 ciclos")
 
-while True:
+for i in range(10):
     led.value(1)
     time.sleep(1)
     led.value(0)
     time.sleep(1)
+
+print("task.py finalizado")
